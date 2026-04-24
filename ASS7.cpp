d69859e04089d2ce666877c06bf964c50c83327b
Bhargav <bbhamare9e20@gmail.com>
2026-04-24 23:17:59 +0530

    Update ASS7.cpp

--- ASS7.cpp
+++ ASS7.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
 using namespace std;
 
-
-
-
-
-
-
-
 // ===================== COMMON FUNCTION =====================
 void printArray(int a[], int n) {
     for (int i = 0; i < n; i++) cout << a[i] << " ";
