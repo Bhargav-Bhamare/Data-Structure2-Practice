d3c3ddf28e4d157e9df275d0dd0094c4690ed4e3
Bhargav <bbhamare9e20@gmail.com>
2026-08-21 23:31:18 +0530

    Update ASS6.cpp

--- ASS6.cpp
+++ ASS6.cpp
@@ -6,6 +6,12 @@ using namespace std;
 
 
 
+
+
+
+
+
+
 // Function to heapify a subtree rooted at index i
 void heapify(int a[], int n, int i) {
     int largest = i;        // Assume root is largest
